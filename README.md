78a4bd03478a5342ed7d1df24d5c1626b861d854
Alexandre Miranda <alexandremirandagomes@gmail.com>
2026-06-05 15:49:34 -0300

    first commit Springboot and maven

--- README.md
+++
@@ -1,1 +0,0 @@
-Estudos do Leet Code
---
+++ parasempredois/parasempredois/src/main/java/com/parasempredois/ParaSempreDoisApplication.java
@@ -0,0 +1,13 @@
+package com.parasempredois;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParaSempreDoisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParaSempreDoisApplication.class, args);
+	}
+
+}
